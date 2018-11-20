d8568cc8d83ab9b2a81ec8434f46d34986b154c9
code76kb <code76kb@gmail.com>
2018-11-20 09:14:17 +0530

    unstable

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ no_hidden_nods_2 = 40
 weight_matrix_1 = 0
 weight_matrix_2 = 0
 weight_matrix_outPut = 0
-bais_1 = 1
-bais_2 = 1
-bais_3 = 1
+bais_1 = 0
+bais_2 = 0
+bais_3 = 0
 
 # input DATA
 mndata = MNIST('/media/patel/DATA/ML_init/DataSets/mnist')
@@ -38,6 +38,10 @@ kernels, paddSize = t.init_kernel(no_kernels,kernel_shape)
 weight_matrix_1 = np.random.uniform(-1,1,(147,no_hidden_nods_1))
 weight_matrix_2 = np.random.uniform(-1,1,(no_hidden_nods_1,no_hidden_nods_2))
 weight_matrix_outPut = np.random.uniform(-1,1,(no_hidden_nods_2,no_output_nods))
+
+bais_1 = np.random.uniform(-1,1,(147,1))
+bais_2 = np.random.uniform(-1,1,(60,1))
+bais_3 = np.random.uniform(-1,1,(40,1))
 # Pooling layer kernel
 pool_kernel = np.zeros(pool_Shape)
 
@@ -61,7 +65,7 @@ def layer_opration(img,kernel):
     return outputImg
 
 # Layer by layer conv+pool run
-def run(img):
+def conv(img):
     print "no of layers: ",no_conv_layer," no kernals: ",no_kernels
     all_layer_output = []
     for i in range(0,no_conv_layer):
@@ -86,21 +90,48 @@ def flattern_data(all_layer_output):
         fc.extend((outputImgs[i]).flatten())
     return np.array(fc).reshape(1,len(fc))
 
+# Sigmoid Activation
 def sigmoid(x):
     return (1 / (1 + np.exp(-x)))
 
-# input
-img = (np.array(images[0],dtype="uint8")).reshape((28,28,1))
-# conved
-all_layer_output = run(img)
-# show
-# show(no_conv_layer,no_kernels)
-#fullConnected
-fc = flattern_data(all_layer_output)
-# nueral net
-hidden_layer_1_out = sigmoid(np.dot(fc, weight_matrix_1)) + bais_1
-hidden_layer_2_out = sigmoid(np.dot(hidden_layer_1_out, weight_matrix_2)) + bais_2
-final_output = sigmoid(np.dot(hidden_layer_2_out, weight_matrix_outPut))
+# Calculate Cost
+def softmax_cost(out,label):
+    eout = np.exp(out,dtype=np.float)
+    probs = eout/eout.sum()
+    label = label.reshape((1,10))
+    p = (label*probs).sum()
+    print "Label shape:",label.shape,"probs shape:",probs.shape
+    cost = -np.log(p)
+    print  "P: ",p
+    print "Probs :",probs
+    print "cost :",cost
+    return cost,props
 
-print "final :",final_output
-print "Arg max: ",final_output.argmax()," max: ",final_output.max()
+
+
+# Prdict
+def predict(img,label):
+
+    # input
+    img = (np.array(img,dtype="uint8")).reshape((28,28,1))
+    # label vector
+    labeles = np.zeros((10,1))
+    labeles[label-1,0] = 1
+
+    # conved
+    all_layer_output = conv(img)
+    # show
+    # show(no_conv_layer,no_kernels)
+    #fullConnected
+    fc = flattern_data(all_layer_output)
+    # nueral net
+    hidden_layer_1_out = sigmoid(np.dot(fc, weight_matrix_1)) + bais_1
+    hidden_layer_2_out = sigmoid(np.dot(hidden_layer_1_out, weight_matrix_2)) + bais_2
+    final_output = sigmoid(np.dot(hidden_layer_2_out, weight_matrix_outPut))
+
+    print "final :",final_output
+    print "Arg max: ",final_output.argmax()," max: ",final_output.max()
+    print "Label: ",label
+    softmax_cost(final_output,labeles)
+
+predict(images[0],labels[0])
